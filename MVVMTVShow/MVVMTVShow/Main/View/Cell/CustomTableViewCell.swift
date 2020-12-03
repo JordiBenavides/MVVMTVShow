17443d7538ebd2dd8352cf715deb98e13fda5c4d
Jordi Milla Catalán <jb.90@hotmail.es>
2020-12-03 18:07:53 +0100

    Custom CustomTableViewCell.

--- MVVMTVShow/MVVMTVShow/Main/View/Cell/CustomTableViewCell.swift
+++ MVVMTVShow/MVVMTVShow/Main/View/Cell/CustomTableViewCell.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import SDWebImage
 
 class CustomTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var labelCell: UILabel!
+    @IBOutlet weak var imageCell: UIImageView!
+    
+    var data:Movie?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +27,11 @@ class CustomTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func configure(movie: Movie?) {
+            
+            self.data = movie
+            labelCell.text = self.data?.name
+            imageCell.sd_setImage(with: URL(string: self.data?.images.medium ?? ""), placeholderImage: UIImage(named: ""))
+        
+    }
 }
